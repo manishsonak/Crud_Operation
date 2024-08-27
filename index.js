76843fb582f80841744cd24a43c3ca20f977d1a6
manishsonak <manishsonak9@gmail.com>
2024-08-27 13:55:17 +0530

    origin change into localhost

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:true}))
 
 
 const corsOptions = {
-    origin: 'https://authncrud.netlify.app/', // Your frontend origin
+    origin: 'http://localhost:5173', // Your frontend origin
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true, // Allow cookies to be sent and received
